4a0dea450b2261a8d530e2d7ffb11dd3d177b0f1
JANI <JANI@gmail.com>
2019-05-28 07:57:33 +0530

    added

---
+++ Hellomaven/src/main/java/com/pasha/App.java
@@ -0,0 +1,13 @@
+package com.pasha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
